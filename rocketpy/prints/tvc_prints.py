eb50069bcf2bd62d4e6bc9d6a955d81772d639c5
ZuoRen Chen <zuorenchen@outlook.com>
2026-03-17 23:45:55 +0000

    Fix tvc prints

--- rocketpy/prints/tvc_prints.py
+++ rocketpy/prints/tvc_prints.py
@@ -1,6 +1,3 @@
-from inspect import getsourcelines
-
-
 class _TVCPrints:
     """Class that contains all TVC prints."""
 
@@ -22,19 +19,10 @@ class _TVCPrints:
         """Prints geometric information of the TVC system."""
         print("Geometric information of the TVC System:")
         print("----------------------------------")
-        print(
-            f"Maximum Gimbal Angle: {self.gimbal_range:.6f} rad "
-            f"({self.gimbal_range:.2f}°)"
-        )
-        print(
-            f"Current Gimbal Angle X: {self.gimbal_angle_x:.6f} rad "
-            f"({self.gimbal_angle_x:.2f}°)"
-        )
-        print(
-            f"Current Gimbal Angle Y: {self.gimbal_angle_y:.6f} rad "
-            f"({self.gimbal_angle_y:.2f}°)"
-        )
-        print(f"Angle Clamping: {'Enabled' if self.clamp else 'Disabled'}")
+        print(f"Maximum Gimbal Angle: {self.tvc.gimbal_range:.2f}°")
+        print(f"Current Gimbal Angle X: {self.tvc.gimbal_angle_x:.2f}°")
+        print(f"Current Gimbal Angle Y: {self.tvc.gimbal_angle_y:.2f}°")
+        print(f"Angle Clamping: {'Enabled' if self.tvc.clamp else 'Disabled'}")
 
     def all(self):
         """Prints all information of the TVC system."""
